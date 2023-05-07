28b87616c1a7cf03cac9c8726c2b50319ebc25fa
thongvm <thongvm@nhanh.vn>
2023-05-07 14:33:21 +0700

    Add project files.

---
+++ HealthInsurance/Models/PolicyOnEmp.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthInsurance.Models
+{
+    public class PolicyOnEmp
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int PolicyId { get; set; }
+
+        [ForeignKey("PolicyId")]
+        public Policy Policy { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+
+        public decimal? EmiSubmitted { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EndDate { get; set; }
+    }
+}
