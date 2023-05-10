171312778465ba4b7942fc225556f0ad22419294
Hieu Vi <hieu.vi@bluebottle.digital>
2023-05-10 21:04:50 +0700

    feat: add static pages
    Signed-off-by: Hieu Vi <hieu.vi@bluebottle.digital>

--- HealthInsurance/Controllers/HomeController.cs
+++ HealthInsurance/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace HealthInsurance.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
