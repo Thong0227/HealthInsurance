15f79a2db734c5271798e4778bdd374db6123d10
Hieu Vi <hieu.vi@bluebottle.digital>
2023-05-21 21:57:08 +0700

    fix: styles
    Signed-off-by: Hieu Vi <hieu.vi@bluebottle.digital>

--- HealthInsurance/Controllers/HomeController.cs
+++ HealthInsurance/Controllers/HomeController.cs
@@ -8,20 +8,6 @@ namespace HealthInsurance.Controllers
         {
             return View();
         }
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
