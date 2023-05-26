bedaea1c873c7c92233600017baecd0c904dacda
thongvm <thongvm@nhanh.vn>
2023-05-26 09:52:42 +0700

    Update regist policy

--- HealthInsurance/Controllers/Registpolicy.cs
+++ HealthInsurance/Controllers/Registpolicy.cs
@@ -17,7 +17,7 @@ namespace HealthInsurance.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             if (User.Identity.IsAuthenticated)
             {
@@ -31,6 +31,35 @@ namespace HealthInsurance.Controllers
             }
             ViewData["PolicyId"] = new SelectList(_context.Policies, "Id", "Name");
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var userName = User.Identity.Name;
+
+                var _emp = _context.Employees.Where(m => m.Email == userName).FirstOrDefault();
+                if (_emp != null)
+                {
+                    ViewData["EmployeeId"] = _emp.Id;
+                }
+            }
+            ViewData["PolicyId"] = new SelectList(_context.Policies, "Id", "Name");
+            return RedirectToAction("Index", "Account");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PolicyId,EmployeeId,EmiSubmitted,StartDate,EndDate,PolicyStatus")] PolicyOnEmp policyOnEmp)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(policyOnEmp);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction("Index", "Registpolicy");
         }
     }
 }
---
+++ HealthInsurance/wwwroot/Assests/js/policyregist.js
@@ -0,0 +1,20 @@
+﻿
+    function registPolicy() {
+        let formData = $('form#registPolicy').serializeArray();
+        formData.push({ name: "EndDate", value: '1900-05-27' });
+        formData.push({ name: "PolicyStatus", value: false });
+        $.ajax({
+            type: 'POST',
+            url: '/RegistPolicy/Create',
+            data: formData,
+            success: function (result) {
+                alert('Successfully received Data ');
+                console.log(result);
+            },
+            error: function () {
+                alert('Failed to receive the Data');
+                console.log('Failed ');
+            }
+        })
+    }
+
