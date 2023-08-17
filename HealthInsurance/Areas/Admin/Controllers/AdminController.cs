0d2cb0f29bb6fa56e9690ab631d307b321b65162
Vu Minh Thong <vuminhthong98@gmail.com>
2023-08-17 23:36:00 +0700

    c

--- HealthInsurance/Areas/Admin/Controllers/AdminController.cs
+++ HealthInsurance/Areas/Admin/Controllers/AdminController.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using Microsoft.AspNetCore.Authorization;
+using HealthInsurance.Data;
 
 namespace HealthInsurance.Controllers
 {
     [Authorize(Roles = "Administrator")]
     public class AdminController : Controller
     {
+        private readonly HealthInsuranceContext _context;
+
+        public AdminController(HealthInsuranceContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var totalEmployee = _context.PolicyOnEmp.Count();
+            ViewBag.totalEmployee = totalEmployee;
             return View();
         }
     }
--- HealthInsurance/Areas/Admin/Controllers/HomeAdminController.cs
+++ HealthInsurance/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using HealthInsurance.Data;
 
 namespace HealthInsurance.Areas.Admin.Controllers
 {
@@ -7,8 +8,22 @@ namespace HealthInsurance.Areas.Admin.Controllers
     [Authorize(Roles = "Administrator")]
     public class HomeAdminController : Controller
     {
+        private readonly HealthInsuranceContext _context;
+
+        public HomeAdminController(HealthInsuranceContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var totalEmployee = _context.PolicyOnEmp.Count();
+            var request = _context.PolicyOnEmp.Where(x => x.PolicyStatus == false).Count();
+            var earning = _context.PolicyOnEmp.Where(x => x.PolicyStatus == true).Sum(x => x.EmiSubmitted);
+            var contact = _context.Contact.Count();
+            ViewBag.contact = contact;
+            ViewBag.earning = earning;
+            ViewBag.totalEmployee = totalEmployee;
+            ViewBag.request = request;
             return View();
         }
     }
